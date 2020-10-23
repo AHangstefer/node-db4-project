e3efbfd99cb5d39ab88e3464480e5468602586e3
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-22 21:21:41 -0400

    seeding recipies

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,23 @@
 module.exports = {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-        filename: "./data/kitchen.db3",
-    },
+    development:{
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/kitchen.db3",
+        },
+        migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: {
+			directory: "./data/seeds",
+		},
 
+		// this is needed when using foreign keys
+		pool: {
+			afterCreate: (conn, done) => {
+				// runs after a connection is made to the sqlite engine
+				conn.run("PRAGMA foreign_keys = ON", done) // turn on FK enforcement
+			},
+		},
+    },
 }
